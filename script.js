52b46ca45a03851a4b72fd3723780de89573b7f3
Manas Choudhary <151459346+ManasChoudharyBITS@users.noreply.github.com>
2024-01-19 20:23:48 +0530

    Update script.js
    Adding scroll animation

--- script.js
+++ script.js
@@ -250,6 +250,22 @@ function myFunction() {
   preloader.style.display = 'none';
 }
 
+// SCROLL ANIMATION
+
+let section = document.querySelectorAll('.section');
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        
+        if(top >= offset - 3.8*height){
+            sec.classList.add('show-animate');
+        }
+        
+    })
+}
+
 
 
 
